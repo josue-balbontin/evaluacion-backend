e9983f36b3bb50cb44f2fd6115448227b92e041c
josue-balbontin <balbontinjosue@gmail.com>
2026-05-21 22:02:36 -0400

    configurando el dejango

--- Django/reservations/admin.py
+++ Django/reservations/admin.py
@@ -9,6 +9,7 @@ class ReservationGuestInline(admin.TabularInline):
     model = ReservationGuest
     extra = 1
     fields = ('full_name', 'email', 'phone', 'dietary_notes', 'is_primary')
+    readonly_fields = ('created', 'modified')
 
 
 class PricingTierInline(admin.TabularInline):
@@ -35,6 +36,7 @@ class RestaurantAdmin(admin.ModelAdmin):
     search_fields = ('name', 'slug', 'address')
     list_filter = ('is_active', 'timezone')
     prepopulated_fields = {'slug': ('name',)}
+    readonly_fields = ('created', 'modified')
     inlines = [TableTypeInline, MenuItemInline]
 
 
@@ -43,6 +45,7 @@ class TableTypeAdmin(admin.ModelAdmin):
     list_display = ('name', 'restaurant', 'seats', 'quantity', 'price_per_seat', 'is_active')
     list_filter = ('restaurant', 'is_active')
     search_fields = ('name', 'restaurant__name')
+    readonly_fields = ('created', 'modified')
     inlines = [PricingTierInline]
 
 
@@ -51,6 +54,7 @@ class PricingTierAdmin(admin.ModelAdmin):
     list_display = ('name', 'table_type', 'price_per_seat', 'priority', 'valid_from', 'valid_until', 'is_active')
     list_filter = ('table_type__restaurant', 'is_active')
     search_fields = ('name', 'table_type__name')
+    readonly_fields = ('created', 'modified')
 
 
 @admin.register(MenuItem)
@@ -58,12 +62,13 @@ class MenuItemAdmin(admin.ModelAdmin):
     list_display = ('name', 'restaurant', 'course', 'price', 'is_available')
     list_filter = ('restaurant', 'course', 'is_available')
     search_fields = ('name', 'description')
-
+    readonly_fields = ('created', 'modified')
 
 @admin.register(ReservationStatus)
 class ReservationStatusAdmin(admin.ModelAdmin):
     list_display = ('name', 'created', 'modified')
     search_fields = ('name',)
+    readonly_fields = ('created', 'modified')
 
 
 @admin.register(Reservation)
@@ -71,6 +76,7 @@ class ReservationAdmin(admin.ModelAdmin):
     list_display = ('customer_name', 'reservation_date', 'reservation_time', 'restaurant', 'table_type', 'status', 'party_size')
     list_filter = ('restaurant', 'status', 'reservation_date', 'table_type')
     search_fields = ('customer_name', 'customer_email', 'customer_phone')
+    readonly_fields = ('created', 'modified')
     inlines = [ReservationGuestInline]
     ordering = ('-reservation_date', '-reservation_time')
 
@@ -80,3 +86,4 @@ class ReservationGuestAdmin(admin.ModelAdmin):
     list_display = ('full_name', 'reservation', 'email', 'is_primary')
     list_filter = ('is_primary',)
     search_fields = ('full_name', 'email')
+    readonly_fields = ('created', 'modified')
